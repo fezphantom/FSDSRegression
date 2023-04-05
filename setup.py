c1b05eda3218b5db9286dda5533064f3174e14e7
Peter Utomakili <fezphantom@gmail.com>
2023-04-05 10:16:46 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str)->List[str]:
+    requirements = list()
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    author='Fezphantom',
+    author_email='fezphantom@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
